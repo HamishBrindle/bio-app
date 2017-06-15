fab939cc61f484ed1c6f45240382e1fe9d652e10
Hamish Brindle <hamishbrindle@gmail.com>
2017-06-15 10:41:35 -0700

    Added bluetooth connection to MainActivity. It may be a good idea to find a way to consolidate this all to a separate class, but for now it should be fine.

--- app/src/main/java/com/example/hamishbrindle/bio_app/Mapping/MappingActivity.java
+++ app/src/main/java/com/example/hamishbrindle/bio_app/Mapping/MappingActivity.java
@@ -2,10 +2,15 @@ package com.example.hamishbrindle.bio_app.Mapping;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.GridLayout;
+import android.widget.Toast;
 
 import com.example.hamishbrindle.bio_app.R;
 import com.example.hamishbrindle.bio_app.Utility.BottomNavigationViewHelper;
@@ -15,19 +20,78 @@ import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
  * Created by hamis on 2017-06-13.
  */
 
-public class MappingActivity extends AppCompatActivity {
+public class MappingActivity extends AppCompatActivity implements MappingView.OnToggledListener {
 
     private static final String TAG = "MappingActivity";
 
     private static final int ACTIVITY_NUM = 0;
 
+    private MappingView[] myViews;
+
+    private GridLayout mappingGridLayout;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mapping);
+
         Log.d(TAG, "onCreate: starting.");
 
+        // Make the bottom navigation bar.
         setupBottomNavigationView();
+
+        // Get the Mapping Grid layout to manipulate.
+        mappingGridLayout = (GridLayout)findViewById(R.id.mappingGrid);
+
+        // Get the number of columns and rows to be displayed in the Mapping Grid.
+        int numOfCol = mappingGridLayout.getColumnCount();
+        int numOfRow = mappingGridLayout.getRowCount();
+
+        // Make an array of all the Squares in the Mapping Grid
+        myViews = new MappingView[numOfCol*numOfRow];
+
+        int gridId = 0;
+
+        for(int yPos=0; yPos<numOfRow; yPos++){
+            for(int xPos=0; xPos<numOfCol; xPos++){
+                MappingView tView = new MappingView(this, xPos, yPos);
+                tView.setOnToggledListener(this);
+                tView.setId(gridId++);
+                myViews[yPos*numOfCol + xPos] = tView;
+                mappingGridLayout.addView(tView);
+            }
+        }
+
+        // Create listeners for each Mapping Grid Square.
+        mappingGridLayout.getViewTreeObserver().addOnGlobalLayoutListener(
+            new ViewTreeObserver.OnGlobalLayoutListener(){
+                @Override
+                public void onGlobalLayout() {
+
+                    // Set the space between each button. I think this should be zero for
+                    // seamless appearance.
+                    final int MARGIN = 0;
+
+                    int pWidth = mappingGridLayout.getWidth();
+                    int pHeight = pWidth;
+                    int numOfCol = mappingGridLayout.getColumnCount();
+                    int numOfRow = mappingGridLayout.getRowCount();
+                    int w = pWidth/numOfCol;
+                    int h = pHeight/numOfRow;
+
+                    for(int yPos=0; yPos<numOfRow; yPos++){
+                        for(int xPos=0; xPos<numOfCol; xPos++){
+                            GridLayout.LayoutParams params =
+                                    (GridLayout.LayoutParams)myViews[yPos*numOfCol + xPos].getLayoutParams();
+                            params.width = w - 2*MARGIN;
+                            params.height = h - 2*MARGIN;
+                            params.setMargins(MARGIN, MARGIN, MARGIN, MARGIN);
+                            myViews[yPos*numOfCol + xPos].setLayoutParams(params);
+                        }
+                    }
+
+                }
+            });
     }
 
     /**
@@ -44,5 +108,18 @@ public class MappingActivity extends AppCompatActivity {
         Menu menu = bottomNavigationViewEx.getMenu();
         MenuItem item = menu.getItem(ACTIVITY_NUM);
         item.setChecked(true);
+    }
+
+    @Override
+    public void OnToggled(MappingView v, boolean touchOn) {
+
+        //get the id string
+        String idString = v.getIdX() + ":" + v.getIdY();
+
+        Toast.makeText(MappingActivity.this,
+                "Toogled:\n" +
+                        idString + "\n" +
+                        touchOn + "",
+                Toast.LENGTH_SHORT).show();
     }
 }
