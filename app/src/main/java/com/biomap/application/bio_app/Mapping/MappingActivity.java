7aaee320af812da1f8b510637304177034a48179
Hamish Brindle <hamishbrindle@gmail.com>
2017-06-23 20:03:10 -0700

    Created the mapping display. Still working out a blank pixel value on the side of the display.

--- app/src/main/java/com/biomap/application/bio_app/Mapping/MappingActivity.java
+++ app/src/main/java/com/biomap/application/bio_app/Mapping/MappingActivity.java
@@ -1,5 +1,6 @@
 package com.biomap.application.bio_app.Mapping;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +9,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.ViewTreeObserver;
 import android.widget.GridLayout;
+import android.widget.Toast;
 
 import com.biomap.application.bio_app.R;
 import com.biomap.application.bio_app.Utility.BottomNavigationViewHelper;
@@ -20,17 +22,25 @@ import java.util.Random;
  * <p>
  * Created by hamis on 2017-06-13.
  */
-public class MappingActivity extends AppCompatActivity {
+public class MappingActivity extends AppCompatActivity implements BitmapSquare.OnToggledListener {
 
     private static final String TAG = "MappingActivity";
 
     private static final int ACTIVITY_NUM = 0;
 
-    private BitmapView[] myViews;
+    private static final int NODES_RESOLUTION = 8;
 
-    private GridLayout mappingGridLayout;
+    private static final int NUM_NODES = (int) Math.pow(NODES_RESOLUTION, 2);
 
-    public static final int MAP_SIZE = 64;
+    public static final int MAP_RESOLUTION = 17;
+
+    public static final int MAP_SIZE = (int) Math.pow(MAP_RESOLUTION, 2);
+
+    private BitmapSquare[][] gridSquares;
+
+    private GridLayout grid;
+
+    private Random rand = new Random();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -41,70 +51,90 @@ public class MappingActivity extends AppCompatActivity {
 
         // Make the bottom navigation bar.
         setupBottomNavigationView();
+        setupGrid();
+    }
+
+    private void setupGrid() {
 
         // Get the Mapping Grid layout to manipulate.
-        mappingGridLayout = (GridLayout) findViewById(R.id.mappingGrid);
+        grid = (GridLayout) findViewById(R.id.mappingGrid);
+
+        // Set the number of columns and rows in the grid.
+        grid.setRowCount(MAP_RESOLUTION);
+        grid.setColumnCount(MAP_RESOLUTION);
 
         // Get the number of columns and rows to be displayed in the Mapping Grid.
-        int numOfCol = mappingGridLayout.getColumnCount();
-        int numOfRow = mappingGridLayout.getRowCount();
+        int numOfCol = grid.getColumnCount();
+        int numOfRow = grid.getRowCount();
 
         // Make an array of all the Squares in the Mapping Grid
-        myViews = new BitmapView[numOfCol * numOfRow];
+        gridSquares = new BitmapSquare[numOfCol][numOfRow];
 
+        // Initialize gridId to start.
         int gridId = 0;
 
+        // Make a mock pressure-chart; this will be 8x8.
         int[] pressure = getPressure();
-        int pressureIndex = 0;
 
+        // Expand the 8x8 pressure inputs to MAP_RESOLUTION.
+        MappingMatrix matrix = new MappingMatrix(pressure, NODES_RESOLUTION, MAP_RESOLUTION);
+        int[][] expandedMatrix = matrix.convertMatrix2D(pressure);
+
+        // TODO: Change expandedMatrix to the new resolution
+        expandedMatrix = matrix.expandMatrix(expandedMatrix);
+
+        Log.e(TAG, "expandedMatrix: " + expandedMatrix);
+
+        // Create squares for the pressure map and add them to the grid. Also, make an array for
+        // the squares so we can make further changes to the grid.
         for (int yPos = 0; yPos < numOfRow; yPos++) {
             for (int xPos = 0; xPos < numOfCol; xPos++) {
-                BitmapView tView = new BitmapView(
+                BitmapSquare tView = new BitmapSquare(
                         this, // View
                         xPos, // X position
                         yPos, // Y Position
-                        pressure[pressureIndex++ % pressure.length] // The pressure value from array
+                        gridId,
+                        expandedMatrix[xPos][yPos] // The pressure value from array
                 );
+                tView.setOnToggledListener(this);
                 tView.setId(gridId++);
-                myViews[yPos * numOfCol + xPos] = tView;
-                mappingGridLayout.addView(tView);
+                gridSquares[xPos][yPos] = tView;
+                grid.addView(tView);
             }
         }
 
         // Create listeners for each Mapping Grid Square.
-        mappingGridLayout.getViewTreeObserver().addOnGlobalLayoutListener(
-            new ViewTreeObserver.OnGlobalLayoutListener() {
-                @Override
-                public void onGlobalLayout() {
+        grid.getViewTreeObserver().addOnGlobalLayoutListener(
+                new ViewTreeObserver.OnGlobalLayoutListener() {
+                    @Override
+                    public void onGlobalLayout() {
+                        // Set the space between each button. Keep at zero.
+                        final int MARGIN = 0;
 
-                    // Set the space between each button. I think this should be zero for
-                    // seamless appearance.
-                    final int MARGIN = 0;
+                        int pWidth = grid.getWidth();
+                        int pHeight = pWidth;
+                        int numOfCol = grid.getColumnCount();
+                        int numOfRow = grid.getRowCount();
+                        int w = pWidth / numOfCol;
+                        int h = pHeight / numOfRow;
 
-                    int pWidth = mappingGridLayout.getWidth();
-                    int pHeight = pWidth;
-                    int numOfCol = mappingGridLayout.getColumnCount();
-                    int numOfRow = mappingGridLayout.getRowCount();
-                    int w = pWidth / numOfCol;
-                    int h = pHeight / numOfRow;
-
-                    for (int yPos = 0; yPos < numOfRow; yPos++) {
-                        for (int xPos = 0; xPos < numOfCol; xPos++) {
-                            GridLayout.LayoutParams params =
-                                    (GridLayout.LayoutParams) myViews[yPos * numOfCol + xPos].getLayoutParams();
-                            params.width = w;
-                            params.height = h;
-                            params.setMargins(MARGIN, MARGIN, MARGIN, MARGIN);
-                            myViews[yPos * numOfCol + xPos].setLayoutParams(params);
+                        for (int yPos = 0; yPos < numOfRow; yPos++) {
+                            for (int xPos = 0; xPos < numOfCol; xPos++) {
+                                GridLayout.LayoutParams params =
+                                        (GridLayout.LayoutParams) gridSquares[xPos][yPos].getLayoutParams();
+                                params.width = w;
+                                params.height = h;
+                                params.setMargins(MARGIN, MARGIN, MARGIN, MARGIN);
+                                gridSquares[xPos][yPos].setLayoutParams(params);
+                            }
                         }
                     }
-                }
-            });
+                });
     }
 
     /**
      * Sets up and enables the bottom navigation bar for each activity.
-     *
+     * <p>
      * Also customizes the bottom navigation so that the buttons don't physically react to being
      * selected. Without this method, the buttons grow and shrink and shift around. It's gross.
      */
@@ -124,11 +154,35 @@ public class MappingActivity extends AppCompatActivity {
      * @return The pressure map array of values.
      */
     private int[] getPressure() {
-        int[] pressure = new int[MAP_SIZE];
-        Random rand = new Random();
-        for (int i = 0; i < MAP_SIZE; i++) {
-            pressure[i] = rand.nextInt(255);
-        }
-        return pressure;
+
+        //        int[] pressure = new int[NUM_NODES];
+//        for (int i = 0; i < NUM_NODES; i++) {
+//            pressure[i] = rand.nextInt(255);
+//        }
+
+        return new int[]{
+            10, 20, 30, 40, 50, 60, 70, 80,
+            20, 30, 40, 50, 60, 70, 80, 10,
+            30, 40, 50, 60, 70, 80, 10, 20,
+            40, 50, 60, 70, 80, 10, 20, 30,
+            50, 60, 70, 80, 10, 20, 30, 40,
+            60, 70, 80, 10, 20, 30, 40, 50,
+            70, 80, 10, 20, 30, 40, 50, 60,
+            80, 10, 20, 30, 40, 50, 60, 70
+        };
     }
+
+    @Override
+    public void OnToggled(BitmapSquare v, boolean touchOn) {
+        //get the id string
+        String idString = v.getXCoordinate() + ":" + v.getYCoordinate();
+
+        v.setColor(Color.rgb(rand.nextInt(255), rand.nextInt(255), rand.nextInt(255)));
+
+        Toast.makeText(MappingActivity.this,
+                "Toogled:\n" +
+                        idString + "\n",
+                Toast.LENGTH_SHORT).show();
+    }
+
 }
